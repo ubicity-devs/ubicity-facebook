b19991659de99992b6dc8008898be72000857efe
Christoph Ruggenthaler <christoph.ruggenthaler@gmail.com>
2015-07-17 11:16:43 +0200

    added test & updated fb version

--- src/test/java/at/ac/ait/ubicity/ubicity/facebook/FBTest.java
+++ src/test/java/at/ac/ait/ubicity/ubicity/facebook/FBTest.java
@@ -2,7 +2,6 @@ package at.ac.ait.ubicity.ubicity.facebook;
 
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import at.ac.ait.ubicity.commons.util.PropertyLoader;
@@ -10,20 +9,18 @@ import at.ac.ait.ubicity.ubicity.facebook.impl.FBPluginImpl;
 import at.ac.ait.ubicity.ubicity.facebook.impl.FbClient;
 
 import com.restfb.Connection;
+import com.restfb.Parameter;
 import com.restfb.types.Page;
 import com.restfb.types.Post;
 
 public class FBTest {
 
 	@Test
-	@Ignore
 	public void ConnectionTest() {
 
-		PropertyLoader config = new PropertyLoader(
-				FBPluginImpl.class.getResource("/facebook.cfg"));
+		PropertyLoader config = new PropertyLoader(FBPluginImpl.class.getResource("/facebook.cfg"));
 
-		FbClient client = new FbClient(config.getString("plugin.fb.api_key"),
-				config.getString("plugin.fb.api_secret"));
+		FbClient client = new FbClient(config.getString("plugin.fb.api_key"), config.getString("plugin.fb.api_secret"));
 
 		Page page = client.fetchObject("Starbucks", Page.class);
 
@@ -31,11 +28,23 @@ public class FBTest {
 
 		client.renewAccessToken();
 
-		Connection<Post> myFeed = client.fetchConnection("Starbucks/feed",
-				Post.class);
+		Connection<Post> myFeed = client.fetchConnection("Starbucks/feed", Post.class);
 
 		// paged list
 		List<Post> posts = myFeed.getData();
 		System.out.println("First item in my feed: " + posts.get(0));
+	}
+
+	@Test
+	public void SearchTest() {
+		PropertyLoader config = new PropertyLoader(FBPluginImpl.class.getResource("/facebook.cfg"));
+
+		FbClient client = new FbClient(config.getString("plugin.fb.api_key"), config.getString("plugin.fb.api_secret"));
+
+		Connection<Page> publicSearch = client.fetchConnection("search", Page.class, Parameter.with("q", "hochwasser"), Parameter.with("type", "page"));
+
+		publicSearch.getData().forEach((entry) -> {
+			System.out.println(entry.getName() + "-" + entry.getAbout());
+		});
 	}
 }
